25707b6cbb1bb54ca8ad374c6e37ac1da818a961
Lunamoon-dot <huyteenpast10@gmail.com>
2025-10-30 22:59:55 +0700

    change sender_email

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -92,8 +92,8 @@ const sendBookingConfirmationEmail = inngest.createFunction(
                     <h2>Hi ${booking.user.name},</h2>
                     <p>Your booking for <strong style="color: #F84565;">"${booking.show.movie.title}"</strong> is confirmed.</p>
                     <p>
-                      <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br/>
-                      <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+                      <strong>Date:</strong> ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Ho_Chi_Minh' })}<br/>
+                      <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Ho_Chi_Minh' })}
                     </p>
                     <p>Enjoy the show! 🍿</p>
                     <p>Thanks for booking with us!<br/>—  OwlCinema</p>
